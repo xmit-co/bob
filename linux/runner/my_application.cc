5ecabe6e740baf205f3a8fa0ba696a21ed652c38
Pierre Carrier <pc@rrier.fr>
2025-12-01 13:14:24 +0000

    v0.0.4

--- linux/runner/my_application.cc
+++ linux/runner/my_application.cc
@@ -46,11 +46,11 @@ static void my_application_activate(GApplication* application) {
   if (use_header_bar) {
     GtkHeaderBar* header_bar = GTK_HEADER_BAR(gtk_header_bar_new());
     gtk_widget_show(GTK_WIDGET(header_bar));
-    gtk_header_bar_set_title(header_bar, "Oncle Bob 0.0.3");
+    gtk_header_bar_set_title(header_bar, "Oncle Bob 0.0.4");
     gtk_header_bar_set_show_close_button(header_bar, TRUE);
     gtk_window_set_titlebar(window, GTK_WIDGET(header_bar));
   } else {
-    gtk_window_set_title(window, "Oncle Bob 0.0.3");
+    gtk_window_set_title(window, "Oncle Bob 0.0.4");
   }
 
   // Minimum width: leftMinPaneWidth (300) + separator (8) + rightMinPaneWidth (600) = 908
