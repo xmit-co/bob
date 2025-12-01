a38ebb15a65af5a12ff79da590ecbe710791e241
Pierre Carrier <pc@rrier.fr>
2025-12-01 13:14:24 +0000

    v0.0.4

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,7 @@ import 'providers/project_provider.dart';
 import 'screens/home_screen.dart';
 import 'services/preferences_service.dart';
 
-const String appVersion = '0.0.3';
+const String appVersion = '0.0.4';
 
 // Global reference for cleanup on app exit
 ProjectProvider? _projectProvider;
